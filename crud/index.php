4341b7c98368f627734a8e7eb60b4bd2d447ffeb
Ankit <ankit181998anand@gmail.com>
2022-04-04 19:41:24 +0530

    second commit

--- crud/index.php
+++ crud/index.php
@@ -12,7 +12,7 @@ require_once 'conect.php';
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Hello, world!</title>
+    <title>Index </title>
 </head>
 
 <body>
@@ -24,6 +24,7 @@ require_once 'conect.php';
                 <i class="fa-solid fa-plus"></i> Add Record
             </button>
         </div>
+        <hr>
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
@@ -33,17 +34,17 @@ require_once 'conect.php';
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <form class="row g-3 needs-validation" novalidate>
+                        <form class="row g-3 needs-validation" novalidate action="" method="POST">
                             <div class="col-md-4">
                                 <label for="validationCustom01" class="form-label">Name</label>
-                                <input type="text" class="form-control" id="validationCustom01" required oninput="this.value=this.value.replace(/[^a-zA-Z .]/g, '').replace(/(\..*)\./g,'$1');">
+                                <input type="text" class="form-control" id="validationCustom01" name="name" required oninput="this.value=this.value.replace(/[^a-zA-Z .]/g, '').replace(/(\..*)\./g,'$1');">
                                 <div class="valid-feedback">
                                     Add your Name properly
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <label for="validationCustom02" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="validationCustom02" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+                                <input type="email" class="form-control" id="validationCustom02" name="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
                                 <div class="valid-feedback">
                                     Enter a valid email
                                 </div>
@@ -51,7 +52,7 @@ require_once 'conect.php';
                             <div class="col-md-4">
                                 <label for="validationCustomUsername" class="form-label">Contact No</label>
                                 <div class="input-group has-validation">
-                                    <input type="text" class="form-control" id="validationCustomUsername" required maxlength="10" pattern="\d{10}" oninput="this.value=this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g,'$1');">
+                                    <input type="text" class="form-control" id="validationCustomUsername" name="contact" required maxlength="10" pattern="\d{10}" oninput="this.value=this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g,'$1');">
                                     <div class="invalid-feedback">
                                         Please write a number.
                                     </div>
@@ -59,18 +60,18 @@ require_once 'conect.php';
                             </div>
                             <div class="col-md-6">
                                 <label for="validationCustom03" class="form-label">Adhaae No</label>
-                                <input type="text" class="form-control" id="validationCustom03" required maxlength="12" pattern="\d{10}" oninput="this.value=this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g,'$1');">
+                                <input type="text" class="form-control" id="validationCustom03" name="adhar" required maxlength="12">
                                 <div class="invalid-feedback">
                                     Please provide a valid city.
                                 </div>
                             </div>
                             <div class="col-md-3">
                                 <label for="validationCustom04" class="form-label">Gender</label>
-                                <select class="form-select" id="validationCustom04" required>
+                                <select class="form-select" id="validationCustom04" name="gender" required>
                                     <option selected disabled value="">Choose...</option>
-                                    <option>Male</option>
-                                    <option>Femle</option>
-                                    <option>Other</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Femle</option>
+                                    <option value="Other">Other</option>
                                 </select>
                                 <div class="invalid-feedback">
                                     Please select a valid state.
@@ -78,13 +79,13 @@ require_once 'conect.php';
                             </div>
                             <div class="col-md-3">
                                 <label for="validationCustom05" class="form-label">Passwprd</label>
-                                <input type="password" class="form-control" id="validationCustom05"  required pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$">
+                                <input type="password" class="form-control" id="validationCustom05" name="password" required pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$">
                                 <div class="invalid-feedback">
                                     Please provide a valid zip.
                                 </div>
                             </div>
                             <div class="col-12">
-                                <button class="btn btn-primary" type="submit">Add</button>
+                                <button class="btn btn-primary" type="submit" name="submit">Add</button>
                             </div>
                         </form>
                     </div>
@@ -117,10 +118,41 @@ require_once 'conect.php';
                 })
         })()
     </script>
-
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
 </body>
 
 </html>
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+    $gender = $_POST['gender'];
+    $adhaar = $_POST['adhar'];
+    $pass = $_POST['password'];
+
+    $qry = "INSERT INTO `user`(`name`, `email`, `contact`, `gender`, `adhar`, `password`) 
+            VALUES ('$name','$email','$contact','$gender','$adhaar','$pass')";
+    $run = mysqli_query($link, $qry);
+
+    if ($run) {
+?>
+        <script>
+            swal("Congratulations!!!!", "Your data inserted successfully", "success");
+        </script>
+    <?php
+    } else {
+    ?>
+        <script>
+            swal("oops!!!!", "Your data not inserted successfully", "error");
+        </script>
+<?php
+    }
+}
+
+
+?>
